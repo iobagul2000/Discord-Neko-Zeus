77f5f9883cd38e4d653213977e933e626a5c7214
SEMAKA <50283032+iobagul2000@users.noreply.github.com>
2022-01-19 16:35:10 +0200

    Add files via upload

---
+++ botconfig.js
@@ -0,0 +1,42 @@
+module.exports = {
+  Admins: ["UserID", "UserID"], //Admins of the bot
+  ExpressServer: true, //If you wanted to make the website run or not
+  DefaultPrefix: process.env.Prefix || "!", //Default prefix, Server Admins can change the prefix
+  Port: 3000, //Which port website gonna be hosted
+  SupportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
+  Token: process.env.Token || "OTI5ODQ2MTg5NzI4MzQ2MTIy.YdtQvw.fK-ohIE0txUsr7fIYaC-v_Gn4bk", //Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "929846189728346122", //Discord Client ID
+  ClientSecret: process.env.Discord_ClientSecret || "PywHv9A6zXHrvst9faa6486QTj44iZDP", //Discord Client Secret
+  Scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
+  ServerDeafen: true, //If you want bot to stay deafened
+  DefaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
+  CallbackURL: "/api/callback", //Discord API Callback url. Do not touch it if you don't know what you are doing. All you need to change for website to work is on line 20.
+  "24/7": false, //If you want the bot to be stay in the vc 24/7
+  CookieSecret: "Pikachu is cute", //A Secret like a password
+  IconURL:
+    "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/logo.gif", //URL of all embed author icons | Dont edit unless you dont need that Music CD Spining
+  EmbedColor: "RANDOM", //Color of most embeds | Dont edit unless you want a specific color instead of a random one each time
+  Permissions: 2205281600, //Bot Inviting Permissions
+  Website: process.env.Website || "http://changeme", //Website where it is hosted at includes http or https || Use "0.0.0.0" if you using Heroku || Do not include /api/callback. Just the website url. I.e. "https://foo.bar"
+  
+  Presence: {
+    status: "online", // You can show online, idle, and dnd
+    name: "Music", // The message shown
+    type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
+  },
+
+  //Lavalink
+  Lavalink: {
+    id: "Main",
+    host: "lavalink.devin-dev.xyz",
+    port: 443, // The port that lavalink is listening to. This must be a number!
+    pass: "lava123",
+    secure: true, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
+  },
+
+  //Please go to https://developer.spotify.com/dashboard/
+  Spotify: {
+    ClientID: process.env.Spotify_ClientID || "054458b579bc4e239cb00affa94366f2", //Spotify Client ID
+    ClientSecret: process.env.Spotify_ClientSecret || "2816cd257d3b4aef966ec86024d3adf5", //Spotify Client Secret
+  },
+};
